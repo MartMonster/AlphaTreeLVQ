c95ccefdcf7e822cd0872a6c81394630a7b8cfce
Mart <78360535+MartMonster@users.noreply.github.com>
2026-06-26 14:17:29 +0200

    prints variety and measurement IDs for all cleansing dataset subfolders

--- readDatabase.py
+++ readDatabase.py
@@ -1,28 +1,44 @@
 import sqlite3
 import os
 
-DATA_DIR = "Croptimal/2024_5_13_CleansingDataset/Run1_light_normal_otherobjects/"
-dirs = [os.path.join(DATA_DIR, "train"), os.path.join(DATA_DIR, "test"), os.path.join(DATA_DIR, "val")]
+DATA_DIR2 = "Croptimal/2024_5_13_CleansingDataset/Run1_light_normal_otherobjects/"
+folders2 = ["train", "test", "val"]
+DATA_DIR = "Croptimal/2024_5_13_CleansingDataset/"
+folders = ["Bladrol", "Dark", "Light", "Normal", "OtherObjects"]
+dirs = [os.path.join(DATA_DIR, folder) for folder in folders]
+dirs2 = [os.path.join(DATA_DIR2, folder) for folder in folders2]
+dirs.extend(dirs2)
 
+dbfile = "Croptimal/2024_5_13_CleansingDataset/Database.db"
+con = sqlite3.connect(dbfile)
+cur = con.cursor()
 for d in dirs:
-    imgs = [f"select Variety from Photos join Measurements on Photos.MeasurementId = Measurements.MeasurementId where Filename is '{f}'" for f in os.listdir(d) if f.lower().endswith('.png')]
-    dbfile = "Croptimal/2024_5_13_CleansingDataset/Database.db"
-    con = sqlite3.connect(dbfile)
-    cur = con.cursor()
+    imgs = [f"select Variety, Photos.MeasurementId from Photos join Measurements on Photos.MeasurementId = Measurements.MeasurementId where Filename is '{f}'" for f in os.listdir(d) if f.lower().endswith('.png')]
 
     variety_counts = {}
+    measurement_ids = {}
     for query in imgs:
         cur.execute(query)
         result = cur.fetchone()
         if result:
             variety = result[0]
+            measurement_id = result[1]
             if variety in variety_counts:
                 variety_counts[variety] += 1
             else:
                 variety_counts[variety] = 1
+            if measurement_id in measurement_ids:
+                measurement_ids[measurement_id] += 1
+            else:
+                measurement_ids[measurement_id] = 1
 
     print(f"Variety counts for {d}:")
     for variety, count in variety_counts.items():
         print(f"  {variety}: {count} ({count / len(imgs) * 100:.2f}%)")
 
-    con.close()
+    print(f"Measurement ID counts for {d}:")
+    for measurement_id, count in measurement_ids.items():
+        print(f"  {measurement_id}: {count} ({count / len(imgs) * 100:.2f}%)")
+
+    print()
+con.close()
